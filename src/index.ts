31c159446a7ca0df2567875564b2c0fca4a15527
Emmanuel Tamburini <emmanueltamburini@gmail.com>
2022-08-24 19:52:00 -0400

    16. Funciones básicas

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,23 @@
 /*
     ===== Código de TypeScript =====
 */
+
+function sumar(a: number, b: number): number {
+    return (a + b);
+}
+
+const sumarFlecha = (a: number, b:number): number => {
+    return a + b;
+}
+
+function multiplicar(numero: number, otroNumer?: number, base: number = 2): number {
+    return numero * base;
+}
+
+const resultado = sumar(10, 20);
+
+const resultado2 = multiplicar(5, 10);
+
+const resultado3 = multiplicar(5, 0, 10);
+
+console.log(resultado);
