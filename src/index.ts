a486829055c3efd65dfd930d22c5016f4c4e1e44
Emmanuel Tamburini <emmanueltamburini@gmail.com>
2022-08-24 20:23:16 -0400

    19. Desestructuración de Objetos

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,36 @@
 /*
     ===== Código de TypeScript =====
 */
+
+interface Reproductor {
+    volumen: number,
+    segundo: number,
+    cancion: string,
+    detalles: Detalles,
+}
+
+interface Detalles {
+    autor: string,
+    anio: number
+}
+
+const reproductor: Reproductor = {
+    volumen: 90,
+    segundo: 36,
+    cancion: 'Mess',
+    detalles: {
+        autor: 'Ed Sheeran',
+        anio: 2015
+    }
+} 
+
+const autor = 'Fulano';
+
+const { volumen, segundo, cancion, detalles: { autor: autorDetalle} } = reproductor;
+// const { autor } = detalles;
+
+
+console.log('El volumen actual de:', volumen);
+console.log('El segundo actual de:', segundo);
+console.log('La canción actual de:', cancion);
+console.log('El autro es:', autorDetalle);
