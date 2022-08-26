01ff9951af0b589131aa16ddb78dd7cc0f7bbeda
Emmanuel Tamburini <emmanueltamburini@gmail.com>
2022-08-25 23:43:10 -0400

    23. Clases básicas

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,17 @@
 /*
     ===== Código de TypeScript =====
 */
+
+class Heroe {
+    alterEgo: string;
+    edad: number;
+    nombreReal: number;
+
+    imprimirNombre() {
+        return this.alterEgo + ' ' + this.nombreReal;
+    }
+}
+
+const ironman = new Heroe();
+
+console.log(ironman);
