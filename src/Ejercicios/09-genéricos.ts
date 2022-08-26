9691efaa1da89e9966870a85c2b9626692597d33
Emmanuel Tamburini <emmanueltamburini@gmail.com>
2022-08-26 00:02:42 -0400

    26. Genéricos

---
+++ src/Ejercicios/09-genéricos.ts
@@ -0,0 +1,12 @@
+/*
+    ===== Código de TypeScript =====
+*/
+
+function queTipoSoy<T>(argumento: T): T {
+    return argumento;
+}
+
+let soyString = queTipoSoy('Hola Mundo');
+let soyNumero = queTipoSoy(100);
+let soyArreglo = queTipoSoy([1,2,3,4,5,6,7,8,9,10]);
+let soyExplicito = queTipoSoy<string>('Hola mundo');
